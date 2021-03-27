ed889dfaea7408689c1ea15303bc7350b872640a
TheRussianOne <81123060+TheRussianOne@users.noreply.github.com>
2021-03-27 11:37:53 -0400

    Script Updated
    Changed from command to keybind.

--- client.lua
+++ client.lua
@@ -1,20 +1,19 @@
-RegisterCommand("ta", function(source, args, rawCommand)
-    local ped = PlayerPedId()
-    local veh = GetVehiclePedIsIn(ped, false)
-
-	if args[1]=="true" then
-		SetVehicleExtra(veh, 4, 1)
-		SetVehicleExtra(veh, 7, 1)
-		SetVehicleExtra(veh, 6, 0)
-		SetVehicleExtra(veh, 8, 0)
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1)
+        if IsControlJustReleased(1,  124) then
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 4, true)
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 7, true)
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 6, false)
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 8, false)
+		alert("~b~Traffic Advisor ~g~ ACTIVE")
+        end
+        if IsControlJustReleased(1,  125) then
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 4, false)
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 7, false)
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 6, true)
+		SetVehicleExtra(GetVehiclePedIsUsing(GetPlayerPed(-1), false), 8, true)
+		alert("~b~Traffic Advisor ~r~ DEACTIVATED")
 	end
-	if args[1]=="false" then
-		SetVehicleExtra(veh, 4, 0)
-		SetVehicleExtra(veh, 7, 0)
-		SetVehicleExtra(veh, 6, 1)
-		SetVehicleExtra(veh, 8, 1)
-    if toggle == "true" then
-        toggle = 1
     end
-    end
-end, false)
+end)
