d8d7850800487dce429c35e371ed9742c2d35f16
TheRussianOne <81123060+TheRussianOne@users.noreply.github.com>
2021-03-27 11:38:32 -0400

    Script Changed
    Script changed to keybind.

---
+++ functions.lua
@@ -0,0 +1,11 @@
+function alert(msg) 
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(msg)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+function notify(string)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(string)
+    DrawNotification(true, false)
+end
